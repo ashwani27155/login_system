f2310bb9911491a47f641b45ad7e7c4263a8f241
infinite-sketch <abic.ashwani@gmail.com>
2021-03-21 05:01:48 -0700

    new update

--- server.js
+++ server.js
@@ -2,7 +2,24 @@
 const express=require('express');
 //load express-handlebars module
 const exphbs=require('express-handlebars');
+//loas body-parser
+const bodyParser=require('body-parser');
+//load mongoose
+const mongoose=require('mongoose');
+//load models
+const Message=require('./models/message');
 const app=express();
+//load keys file
+const Keys=require('./config/keys');
+//use body parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+//connect to mlab(database)
+mongoose.connect(Keys.MongoDB,{useUnifiedTopology:true,useNewUrlParser:true}).then(()=>{
+    console.log('Server is connected to MongoDB');
+}).catch((error)=>{
+    console.log(error);
+});
 //environment variable for port
 
 const port=process.env.PORT || 3000;
@@ -25,6 +42,10 @@ app.get('/contact',(req,res)=>{
     res.render('contact',{
         title:'Contact'
     });
+});
+app.post('/contactus',(req,res)=>{
+    console.log(req.body);
+    //
 });
 app.listen(port,()=>{
     console.log('Server is running on port '+port);
